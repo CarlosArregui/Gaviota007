706b1c30879c803093704af18af025ddbe01b5e3
jorgeperezL <ovejo_jorge@hotmail.com>
2019-02-01 20:25:10 +0100

    Diseño

--- app/src/main/java/com/example/carre/gaviota007/ActivityLogin.java
+++ app/src/main/java/com/example/carre/gaviota007/ActivityLogin.java
@@ -1,14 +1,27 @@
 package com.example.carre.gaviota007;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityLogin extends AppCompatActivity {
-
+    Button sign_up,log_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        sign_up =(Button)findViewById(R.id.sign_up);
+        final Intent I = new Intent(this,Registro.class);
+        View.OnClickListener listar = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(I);
+            }
+        };
+        sign_up.setOnClickListener(listar);
     }
 }
---
+++ app/src/main/java/com/example/carre/gaviota007/Registro.java
@@ -0,0 +1,17 @@
+package com.example.carre.gaviota007;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+        getSupportActionBar().hide();
+    }
+}
