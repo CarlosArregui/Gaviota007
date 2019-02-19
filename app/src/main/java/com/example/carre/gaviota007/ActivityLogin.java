3a5307eaa9305d8611e35457756f97a265b4483c
Gonzalo <gonzalo224455@gmail.com>
2019-02-19 16:29:27 +0100

    Terminada la funcionalidad de Login

--- app/src/main/java/com/example/carre/gaviota007/ActivityLogin.java
+++ app/src/main/java/com/example/carre/gaviota007/ActivityLogin.java
@@ -3,6 +3,7 @@ package com.example.carre.gaviota007;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -16,6 +17,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -28,6 +30,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.dynamic.IFragmentWrapper;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -36,6 +39,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import io.paperdb.Paper;
+
 /**
  * Activity de inicio de sesión.
  */
@@ -43,6 +48,7 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class ActivityLogin extends AppCompatActivity {
 
     //Atributos de la clase
+    private CheckBox checkBoxRememberMe;
     private Context context;
     private Button sign_up,log_in;
     private EditText emailLog;
@@ -54,7 +60,8 @@ public class ActivityLogin extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private  String idCliente;
     private static final int RC_SIGN_IN = 9001;
-
+    private SharedPreferences mPrefs;
+    private static final String prefs_name="PrefsFile";
     /*
      * En el onCreate inicializamos los atributos, añadimos los listener de los botones y otras cosas
      * de firebase.
@@ -72,6 +79,8 @@ public class ActivityLogin extends AppCompatActivity {
         sign_up =(Button)findViewById(R.id.sign_up);
         log_in =(Button)findViewById(R.id.log_in);
         passforget=(TextView)findViewById(R.id.tvPassForget);
+        checkBoxRememberMe=(CheckBox)findViewById(R.id.cbRemember);
+
         //Este botón esta destinado al inicio de sesión de google
 
         signInButton = findViewById(R.id.signInButton);
@@ -121,10 +130,36 @@ public class ActivityLogin extends AppCompatActivity {
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        /*
+         * Se inicializa el objeto de SharedPreferences y se pasan por parametros prefs_name que indica el nombre de la colección
+          * MODE_PRIVATE donde indicamos que solo nuestra aplicacion tiene acceso a nuestras preferencias
 
+         */
 
+        mPrefs=getSharedPreferences(prefs_name,MODE_PRIVATE);
+        getPreferencesData();
 
     }
+    /*
+     * En este método se modifica la coleccion con getters y setters añadiendolos con las keys a los EditText
+     */
+
+    private void getPreferencesData() {
+        SharedPreferences sharedPreferences=getSharedPreferences(prefs_name,MODE_PRIVATE);
+        if (sharedPreferences.contains("pref_name")){
+            String u=sharedPreferences.getString("pref_name", "not found");
+            emailLog.setText(u);
+        }
+        if (sharedPreferences.contains("pref_pass")){
+            String p=sharedPreferences.getString("pref_pass", "not found");
+            passLog.setText(p);
+        }
+        if (sharedPreferences.contains("checkbox")){
+            Boolean b=sharedPreferences.getBoolean("checkbox", false);
+            checkBoxRememberMe.setChecked(b);
+        }
+    }
+
 
     /**
      * El método de LogearUsuario() obtiene los Strings ingresados en el campo de email y pass para poder realizar el logeo.
@@ -135,6 +170,7 @@ public class ActivityLogin extends AppCompatActivity {
 
         String correo=emailLog.getText().toString().trim();
         String contra=passLog.getText().toString().trim();
+
 
         /*
          * Si el campo de correo o de contraseña están vacios llaman al método snackbar() que crea un
@@ -149,7 +185,23 @@ public class ActivityLogin extends AppCompatActivity {
             snackbar("Ingresa una contraseña");
             return;
         }
-
+        /*
+         *Esta condición se cumple si el checkbox es true, al ser true se crea un objeto de SharedPreferences editor
+         * para editar en este caso crear tres keys, el nombre, la contraseña y el checkbox cogiendo los datos
+         * de los Edit Text y el Checkbox complementando y completando la coleccion de mPrefs, en el caso de que
+         * el checkbox no sea true, se limpia mPrefs.
+         */
+        if (checkBoxRememberMe.isChecked()){
+            Boolean boolIsCheked=checkBoxRememberMe.isChecked();
+            SharedPreferences.Editor editor=mPrefs.edit();
+            editor.putString("pref_name", correo);
+            editor.putString("pref_pass", contra);
+            editor.putBoolean("checkbox", boolIsCheked);
+            editor.apply();
+            Toast.makeText(ActivityLogin.this, "Guardado", Toast.LENGTH_LONG).show();
+        }else{
+            mPrefs.edit().clear().apply();
+        }
         /*
          * mAuth llama al metodo signInWithEmailAndPassword() donde se pasan como parametros el correo
          * y la contraseña. Una vez hecho se comprueban los datos que estén en Firebase. Se crea un objeto tipo
@@ -166,7 +218,7 @@ public class ActivityLogin extends AppCompatActivity {
                             snackbar("No has verificado el correo");
                         }else{
                             if (task.isSuccessful()) {
-
+                                snackbar("Logeado");
 
 
                             } else{
@@ -175,6 +227,8 @@ public class ActivityLogin extends AppCompatActivity {
                         }
                     }
                 });
+        emailLog.getText().clear();
+        passLog.getText().clear();
     }
     /*
      * En este metodo se crea un Intent y llama al predefinido de google para visualizar tus cuentas y lo inicializa
@@ -270,4 +324,5 @@ public class ActivityLogin extends AppCompatActivity {
         AlertDialog b=dialog.create();
         b.show();
     }
+
 }
